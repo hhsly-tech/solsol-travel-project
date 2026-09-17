a5b067bb65caa9bed0f28d00cda6984a1bd784ab
hhsly-tech <hhsly-tech@users.noreply.github.com>
2026-09-17 12:44:04 +0900

    Connect shared Apps Script storage

--- api-config.js
+++ api-config.js
@@ -1,3 +1,3 @@
 // Google Apps Script 웹 앱을 배포한 뒤 생성된 /exec 주소를 입력하세요.
 // 예: window.SOLSOL_API_URL = "https://script.google.com/macros/s/배포ID/exec";
-window.SOLSOL_API_URL = "";
+window.SOLSOL_API_URL = "https://script.google.com/macros/s/AKfycbzMHTk4quv9_Y3C5Fo_xSq7gFJNjbgDX-58_ROrjaUkm0vdeE3npvG_Z_LHXNUNJmJ0/exec";
